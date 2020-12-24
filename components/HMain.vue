475d0605a7bd967db73cdd2aecd9ccf646975037
akbarSalahshour <akbarsalahshour3230@gmail.com>
2020-12-24 13:29:57 +0330

    create h-main component and add bg-header to assets

---
+++ components/HMain.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="pt-3">
+    <div class="d-block">
+      <h-navbar></h-navbar>
+    </div>
+  </header>
+</template>
+<style lang="scss">
+header {
+  position: relative;
+  z-index: 9;
+  background-image: url("~assets/img/bg-header.jpg");
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vw;
+  max-height: 90vh;
+  min-height: 300px;
+  & > div{
+      padding:0 1rem;
+      max-width:1200px;
+      margin:auto;
+  }
+}
+</style>
