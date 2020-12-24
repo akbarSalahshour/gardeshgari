cf2196d3dbf5e6f9006f1be439dcf19375b1a712
akbarSalahshour <akbarsalahshour3230@gmail.com>
2020-12-24 17:12:10 +0330

    add font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '~assets/scss/style.scss'
+    '~assets/scss/style.scss',
+    '@fortawesome/fontawesome-free/css/all.css',
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
